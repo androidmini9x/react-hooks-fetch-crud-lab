a94d8ed7aab8d88768dbe9bb242d9120efef1897
androidmini9x <androidmini92@gmail.com>
2022-01-24 01:09:24 +0300

    Solved

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((resp) => resp.json())
+    .then((data) => {
+      setQuestions(data);
+    });
+  }, []);
+
+  function deleteQuiz(id) {
+    fetch("http://localhost:4000/questions/"+id, {
+      method: "DELETE"
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setQuestions(questions.filter((quiz) => quiz.id !== id))
+    });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.length > 0 && questions.map(quiz => {
+        return <QuestionItem key={quiz.id} question={quiz} onDeleteQuiz={deleteQuiz} />
+      })}</ul>
     </section>
   );
 }
